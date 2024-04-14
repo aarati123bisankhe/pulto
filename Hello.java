496a212da3e39eeb054328d05e0d8c231772cd88
aarati <aarati123bisankhe@gmail.com>
2024-04-14 12:17:21 +0545

    week 1 comment

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        /* Write a program that prints Hello World! to the console. Save the program as Hello.java. */
     }
 }
